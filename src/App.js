4bcd064323cac8ac4231d43797490218de54cbe7
Daniel Kish <aox.soljah@gmail.com>
2021-11-02 05:31:56 +0700

    resume display test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import "./App.css";
 import { Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import pdf from "./assets/resume.pdf";
-import { render } from "@testing-library/react";
 
 function App() {
   const onResumeClick = () => {
     window.open(pdf);
   };
 
-  render();
   return (
     <div className="App">
       <Typography variant="h1">Hi welcome hi hello</Typography>
