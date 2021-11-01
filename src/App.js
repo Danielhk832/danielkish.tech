441270c5b2e003d68499e11d388be945f6159ac0
Daniel Kish <aox.soljah@gmail.com>
2021-11-01 23:42:32 +0700

    rough resume display logic setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import { Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import pdf from "./assets/resume.pdf";
+import { render } from "@testing-library/react";
 
 function App() {
-  const onDownload = () => {
-    const link = document.createElement("a");
-    link.download = "resume.pdf";
-    link.href = "./assets/resume.pdf";
-    link.click();
+  const onResumeClick = () => {
+    window.open(pdf);
   };
 
+  render();
   return (
     <div className="App">
       <Typography variant="h1">Hi welcome hi hello</Typography>
-      <Button onClick={onDownload}>Resume</Button>
+      <Button onClick={onResumeClick}>click me BABYYYY</Button>
     </div>
   );
 }
