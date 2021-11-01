4ee1cf7cefc0d56155f356cb36fdad91a61903f8
Daniel Kish <aox.soljah@gmail.com>
2021-11-01 09:07:01 +0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  const onDownload = () => {
+    const link = document.createElement("a");
+    link.download = "resume.pdf";
+    link.href = "./assets/resume.pdf";
+    link.click();
+  };
+
+  return (
+    <div className="App">
+      <Typography variant="h1">Hi welcome hi hello</Typography>
+      <Button onClick={onDownload}>Resume</Button>
+    </div>
+  );
 }
 
 export default App;
